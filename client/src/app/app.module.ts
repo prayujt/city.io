d9116c83fa28b6414cd28a42cc1db4b875de925f
Michael Tu <michaeltu12345@gmail.com>
2023-02-03 18:19:33 -0500

    made first api request, added password confirmation, tweaked colors (#6,#9)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import { MatIconModule } from '@angular/material/icon';
     AngularMaterialModule,
     FlexLayoutModule,
     FormsModule, ReactiveFormsModule,
+    HttpClientModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
+@Injectable()
 export class RegisterComponent {
-  Roles: any = ['Admin', 'Author', 'Reader'];
-  constructor() { }
-  ngOnInit() {
+  constructor(private http: HttpClient) {}
+  ngOnInit() {}
+  public getData(username: string, password: string, password2: string): void {
+    if (password == password2){
+      this.http.post<any>('http://prayujt.com:8000/login/createAccount', {
+        username: username,
+        password: password
+      }).subscribe((response) => {
+        console.log(response);
+      })
+    }
   }
 }
