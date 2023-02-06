552a127ce07cc5344f346ffdf2c351ad4fbb5d5c
Michael Tu <michaeltu12345@gmail.com>
2023-02-06 15:17:29 -0500

    set up conditional routing between register/login (#9)

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: []
 })
 export class AppRoutingModule { }
--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import {CanActivate, Router, RouterStateSnapshot, ActivatedRouteSnapshot} from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
@@ -9,15 +12,36 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class RegisterComponent {
-  constructor(private http: HttpClient) {}
-  ngOnInit() {}
+  constructor(
+    private http: HttpClient, 
+    private _snackBar: MatSnackBar, 
+    private router: Router
+  ) {};
+  ngOnInit() {};
+  public showPassword: boolean = false;
+  public toggleVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
   public getData(username: string, password: string, password2: string): void {
-    if (password == password2){
+    if (username.length == 0){
+      this._snackBar.open("Input a valid username!", "Close", {duration: 2000});
+    } else if (password.length == 0) {
+      this._snackBar.open("Input a valid password!", "Close", {duration: 2000});
+    } else if (password != password2) {
+      this._snackBar.open("Passwords must match!", "Close", {duration: 2000});
+    } else {
+      console.log("Valid Password")
       this.http.post<any>('http://prayujt.com:8000/login/createAccount', {
         username: username,
         password: password
       }).subscribe((response) => {
         console.log(response);
+        if (!response) {
+          this._snackBar.open("This username is not available!", "Close", {duration: 2000});
+        } else {
+          this._snackBar.open("Account successfully created!", "Close", {duration: 2000});
+          this.router.navigate(['login']);
+        }
       })
     }
   }
