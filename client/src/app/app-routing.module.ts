aacde612a9477d3a6ad6b8a2021d43e0971c1cbe
Prayuj Tuli <prayujtuli@hotmail.com>
2023-02-06 15:41:11 -0500

    changed prettier linter to use 4 spaces instead of tabs

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 const routes: Routes = [
-	{ path: '', pathMatch: 'full', redirectTo: 'login' },
-	{ path: 'login', component: LoginComponent },
-	{ path: 'register', component: RegisterComponent },
+    { path: '', pathMatch: 'full', redirectTo: 'login' },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
 ];
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
-	exports: [RouterModule],
-	providers: [],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+    providers: [],
 })
 export class AppRoutingModule {}
