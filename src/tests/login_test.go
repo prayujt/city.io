f8bf715e8355d0de3ffaa90c3143527f9df39cce
Prayuj Tuli <prayujtuli@hotmail.com>
2023-02-11 16:36:56 -0500

    added more login tests (#36)

--- src/tests/login_test.go
+++ src/tests/login_test.go
@@ -20,6 +20,19 @@ func TestAccountCreate(t *testing.T) {
 	}
 }
 
+func TestDuplicateAccount(t *testing.T) {
+	acc := login.Account{
+		Username: "test",
+		Password: "test",
+	}
+
+	body := Post("/login/createAccount", acc)
+
+	if string(body) != "false" {
+		t.Error("Expected duplicate account creation to return false, instead got true")
+	}
+}
+
 func TestAccountLogin(t *testing.T) {
 	acc := login.Account{
 		Username: "test",
@@ -32,6 +45,54 @@ func TestAccountLogin(t *testing.T) {
 	json.Unmarshal(body, &session)
 
 	if !session.Status {
-		t.Error("Expected session to return true, instead got false")
+		t.Error("Expected session creation to return true, instead got false")
+	}
+}
+
+func TestIncorrectUsername(t *testing.T) {
+	acc := login.Account{
+		Username: "test2",
+		Password: "test",
+	}
+
+	body := Post("/login/createSession", acc)
+
+	var session login.Session
+	json.Unmarshal(body, &session)
+
+	if session.Status {
+		t.Error("Expected session creation with incorrect username to return false, instead got true")
+	}
+}
+
+func TestIncorrectPassword(t *testing.T) {
+	acc := login.Account{
+		Username: "test",
+		Password: "test2",
+	}
+
+	body := Post("/login/createSession", acc)
+
+	var session login.Session
+	json.Unmarshal(body, &session)
+
+	if session.Status {
+		t.Error("Expected session creation with incorrect password to return false, instead got true")
+	}
+}
+
+func TestIncorrectUsernamePassword(t *testing.T) {
+	acc := login.Account{
+		Username: "testing",
+		Password: "testing",
+	}
+
+	body := Post("/login/createSession", acc)
+
+	var session login.Session
+	json.Unmarshal(body, &session)
+
+	if session.Status {
+		t.Error("Expected session creation with incorrect username and password to return false, instead got true")
 	}
 }
