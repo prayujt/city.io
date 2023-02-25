b0482a94bec05c74300fb6ebe92bf006c256be4f
Michael Tu <michaeltu12345@gmail.com>
2023-02-24 22:48:25 -0500

    added progress bar for building construction

--- client/src/app/components/game/game.component.ts
+++ client/src/app/components/game/game.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 import { environment } from '../../../environments/environment';
 import { CookieService } from 'ngx-cookie-service';
@@ -109,8 +108,15 @@ export class GameComponent {
     endTime!: string;
     clicked: boolean = false;
     progBar: boolean = false;
+
     startUnix!: number;
     endUnix!: number;
+    progress!: number;
+    interval: any;
+    remaining!: number;
+    hh!: number;
+    mm!: number;
+    ss!: number;
 
     onTileClick(row: number, column: number) {
         // show buildable buildings
@@ -122,6 +128,7 @@ export class GameComponent {
         tile?.classList.add('selected');
 
         // update sidebar stats
+        clearInterval(this.interval);
         this.http
             .get<any>(
                 `http://${environment.API_HOST}:${environment.API_PORT}/cities/${this.ID}/buildings/${row}/${column}`
@@ -135,12 +142,24 @@ export class GameComponent {
                 this.endTime = response.endTime;
             }))
         this.clicked = true;
-        if (this.startTime != null && this.endTime != null) {
-            this.progBar = true;
-            this.startUnix = Date.parse(this.startTime);
-            this.endUnix = Date.parse(this.endTime);
-        }
-        else this.progBar = false;
-        console.log("clicky")
+        
+        this.interval = setInterval(() => {
+            if (this.startTime != "" && this.endTime != "") {
+                this.progBar = true;
+                this.startUnix = Date.parse(this.startTime);
+                this.endUnix = Date.parse(this.endTime);
+                let time: number = Date.now();
+                this.progress = (time-this.startUnix) / (this.endUnix-this.startUnix) * 100;
+                if (this.progress > 100) this.progBar = false;
+                this.remaining = this.endUnix-time;
+                this.ss = this.remaining / 1000;
+                this.hh = Math.floor(this.ss / 3600);
+                this.ss %= 3600;
+                this.mm = Math.floor(this.ss / 60);
+                this.ss %= 60;
+                this.ss = Math.floor(this.ss);
+            }
+            else this.progBar = false;
+        }, 100);
     }
 }
