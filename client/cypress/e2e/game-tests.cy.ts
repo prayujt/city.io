f3be11e4ae627af50555a359d339bb6d16846f09
Michael Tu <michaeltu12345@gmail.com>
2023-03-29 13:49:25 -0400

    added some tests

---
+++ client/cypress/e2e/game-tests.cy.ts
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+describe('Game tests', () => {
+    beforeEach(() => {
+        cy.visit('localhost:4200/login');
+        let username: string = "train"
+        let password: string = "train"
+        cy.get("input[placeholder='Username']").type(username);
+        cy.get("input[placeholder='Password']").type(password);
+        cy.get("button").contains("Log in").click();
+    })
+
+    it('Train Troops', () => {
+        cy.get("button").contains("Log in").click();
+        cy.get("a[matTooltip='Train Troops']").click();
+        cy.get("input[matSliderThumb]").focus().type("{rightarrow}");
+        cy.get("a").contains("Train").click();
+        cy.get("a[matTooltip='Train Troops']").click();
+        cy.get("mat-progress-bar");
+    })
+
+    it('Change City Name', () => {
+        cy.get("a[matTooltip='Edit City Name']").click();
+        cy.get("input[placeholder='City Name']").type("Cypress City");
+        cy.get("button").contains("Change Name").click();
+        cy.get("a").contains("Cypress City");
+    })
+
+    it("Attack and View March", () => {
+        cy.get("td[data-id='1 1']").click();
+    })
+  })
+  
