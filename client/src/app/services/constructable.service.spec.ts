c4ad0f2350819d44c064990579cd747e9be633f5
Prayuj Tuli <prayujtuli@hotmail.com>
2023-03-29 16:42:27 -0400

    fixed front-end unit tests

--- client/src/app/services/constructable.service.spec.ts
+++ client/src/app/services/constructable.service.spec.ts
@@ -1,16 +1,20 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { ConstructableService } from './constructable.service';
 
 describe('ConstructableService', () => {
-  let service: ConstructableService;
+    let service: ConstructableService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ConstructableService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [HttpClientTestingModule],
+        });
+        service = TestBed.inject(ConstructableService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
--- client/src/app/services/constructable.spec.ts
+++ client/src/app/services/constructable.spec.ts
@@ -1,7 +1,15 @@
 import { Constructable } from './constructable';
 
 describe('Constructable', () => {
-  it('should create an instance', () => {
-    expect(new Constructable()).toBeTruthy();
-  });
+    it('should create an instance', () => {
+        expect(
+            new Constructable({
+                buildingType: 'test',
+                buildCost: 0,
+                buildTime: 0,
+                buildingProduction: 0,
+                happinessChange: 0,
+            })
+        ).toBeTruthy();
+    });
 });
