57526214242ac632225847cc8d4b6baa2b2b880c
JJiang28 <jiangjason28@gmail.com>
2023-04-19 00:25:20 -0400

    testcases

--- src/tests/army_test.go
+++ src/tests/army_test.go
@@ -3,13 +3,15 @@ package tests
 import (
 	"api/database"
 	"api/game"
+	"api/login"
 	"time"
 
 	"encoding/json"
+	"fmt"
 	"testing"
 )
 
-func TestArmyTrain(t *testing.T) {
+func TestArmyTrainFail(t *testing.T) {
 	train := game.Train{
 		TroopCount: 1,
 	}
@@ -58,4 +60,59 @@ func TestStartMarchAttack(t *testing.T) {
 	}
 
 	database.Execute("DELETE FROM Training")
+}
+
+func TestArmyTrainSuccess(t *testing.T) {
+	acc4 := login.Account{
+		Username: "rawr4",
+		Password: "rawr4",
+	}
+
+	Post("/login/createAccount", acc4)
+
+	response := Post("/login/createSession", acc4)
+
+	var session3 login.JWT
+	json.Unmarshal(response, &session3)
+
+	if session3.Token == "" {
+		fmt.Println("Failed to initialize token for tests")
+		return
+	}
+
+	building := game.Building{
+		BuildingType:  "Barracks",
+		BuildingLevel: 1,
+		CityRow:       0,
+		CityColumn:    0,
+	}
+
+	response2 := Post("/cities/createBuilding", building)
+	var result2 game.Status
+	json.Unmarshal(response2, &result2)
+
+	if !result2.Status {
+		t.Error("Expected to succeed in creating building")
+	}
+
+	train := game.Train{
+		TroopCount: 1,
+	}
+
+	response3 := Post("/armies/train", train)
+	var result3 game.Status
+	json.Unmarshal(response3, &result3)
+
+	if !result3.Status {
+		t.Error("Expected to succeed")
+	}
+
+	response = Get("/armies/training")
+
+	var trainingResult game.Training
+	json.Unmarshal(response, &trainingResult)
+
+	if trainingResult.ArmySize != 1 {
+		t.Error("Expected army size of 1")
+	}
 }
