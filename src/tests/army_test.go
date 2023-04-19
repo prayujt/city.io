6f59fc2eddf581666f11c7cf7ceed0ff0a5a8e4c
JJiang28 <jiangjason28@gmail.com>
2023-04-19 00:41:12 -0400

    more tests lol

--- src/tests/army_test.go
+++ src/tests/army_test.go
@@ -11,22 +11,6 @@ import (
 	"testing"
 )
 
-func TestArmyTrainFail(t *testing.T) {
-	train := game.Train{
-		TroopCount: 1,
-	}
-
-	response := Post("/armies/train", train)
-	var result game.Status
-	json.Unmarshal(response, &result)
-
-	time.Sleep(time.Second * 1)
-
-	if result.Status {
-		t.Error("Expected not to succeed")
-	}
-}
-
 func TestMarch(t *testing.T) {
 	response := Get("/armies/marches")
 	var result []game.March
@@ -37,29 +21,96 @@ func TestMarch(t *testing.T) {
 	}
 }
 
-func TestStartMarchAttack(t *testing.T) {
+func TestStartMarchAttackPass(t *testing.T) {
+	acc5 := login.Account{
+		Username: "rawr5",
+		Password: "rawr4",
+	}
+
+	Post("/login/createAccount", acc5)
+
+	response := Post("/login/createSession", acc5)
+
+	var session3 login.JWT
+	json.Unmarshal(response, &session3)
+
+	if session3.Token == "" {
+		fmt.Println("Failed to initialize token for tests")
+		return
+	}
+
+	city := game.CityNameChange{
+		CityNameNew: "monkee3",
+	}
+
+	response1 := Post("/cities/updateName", city, session3.Token)
+	var result game.Status
+
+	json.Unmarshal(response1, &result)
+
+	if !result.Status {
+		t.Error("Expected to succeed in changing name")
+	}
+
+	building := game.Building{
+		BuildingType:  "Barracks",
+		BuildingLevel: 1,
+		CityRow:       0,
+		CityColumn:    0,
+	}
+
+	response2 := Post("/cities/createBuilding", building)
+	var result2 game.Status
+	json.Unmarshal(response2, &result2)
+
+	if !result2.Status {
+		t.Error("Expected to succeed in creating building")
+	}
+
 	train := game.Train{
 		TroopCount: 1,
 	}
 
-	response1 := Post("/armies/train", train)
-	var result1 game.Status
-	json.Unmarshal(response1, &result1)
+	response3 := Post("/armies/train", train)
+	var result3 game.Status
+	json.Unmarshal(response3, &result3)
 
-	if result1.Status {
-		t.Error("Expected to succeed in training army")
+	if !result3.Status {
+		t.Error("Expected to succeed")
 	}
 
-	response1 = Get("/armies/training")
+	response = Get("/armies/training")
 
 	var trainingResult game.Training
-	json.Unmarshal(response1, &trainingResult)
+	json.Unmarshal(response, &trainingResult)
 
-	if trainingResult.ArmySize != 0 {
+	if trainingResult.ArmySize != 1 {
 		t.Error("Expected army size of 1")
 	}
 
 	database.Execute("DELETE FROM Training")
+
+	march := game.March{
+		ArmySize: 1,
+		FromCity: "monkee monkee",
+		ToCity:   "monkee",
+	}
+
+	response4 := Post("/armies/move", march)
+	var result4 game.Status
+	json.Unmarshal(response4, &result4)
+
+	if !result4.Status {
+		t.Error("Expected to succeed in starting march")
+	}
+
+	response4 = Get("/armies/marches")
+	var marchResult []game.March
+	json.Unmarshal(response4, &marchResult)
+
+	if marchResult[0].IsAttack != true {
+		t.Error("Expected an attack")
+	}
 }
 
 func TestArmyTrainSuccess(t *testing.T) {
@@ -114,5 +165,21 @@ func TestArmyTrainSuccess(t *testing.T) {
 
 	if trainingResult.ArmySize != 1 {
 		t.Error("Expected army size of 1")
+	}
+}
+
+func TestArmyTrainFail(t *testing.T) {
+	train := game.Train{
+		TroopCount: 1,
+	}
+
+	response := Post("/armies/train", train)
+	var result game.Status
+	json.Unmarshal(response, &result)
+
+	time.Sleep(time.Second * 1)
+
+	if result.Status {
+		t.Error("Expected not to succeed")
 	}
 }
